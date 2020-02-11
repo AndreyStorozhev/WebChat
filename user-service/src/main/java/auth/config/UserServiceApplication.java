98f51015b62f8d8ad836d23210f114fe9c101c67
astorojev <astorojev@mmtr.ru>
2020-02-11 15:46:59 +0300

    intermediate result

--- user-service/src/main/java/auth/config/UserServiceApplication.java
+++ user-service/src/main/java/auth/config/UserServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableEurekaClient
 @EnableJpaRepositories("auth.dao")
 @EntityScan("auth.entity")
+@EnableZuulProxy
 @ComponentScan({"auth.controllers", "auth.service", "auth.validator"})
 @Import({WebSecurityConfig.class, OtherConfig.class})
 public class UserServiceApplication {
